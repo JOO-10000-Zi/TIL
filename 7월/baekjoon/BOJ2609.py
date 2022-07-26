2c99e07e2e43fa1b07a3541fb94edca45cee26a0
JOO-10000-Zi <j4615545@gmail.com>
2022-07-26 18:04:56 +0900

    백준 문제풀이

---
+++ 7월/baekjoon/BOJ2609.py
@@ -0,0 +1,22 @@
+a, b = map(int, input().split())
+
+# 인수로 받은 각가의 수를 그 수만큼 1부터 순회하는 값으로 나누어
+# 0이 되는 값을 약수로 추출해 낸다
+
+a1 = []
+b1 = []
+for i in range(1, a+1):
+    if a%i == 0:
+        a1.append(i)
+
+for j in range(1, b+1):
+    if b%j == 0 :
+        b1.append(j)
+
+yeak = [i for i in a1 if i in b1]
+max_yeak = max(yeak)
+a2 = a//max_yeak
+b2 = b//max_yeak
+min_yeak = a2 * b2 * max_yeak
+print(max_yeak)
+print(min_yeak)
