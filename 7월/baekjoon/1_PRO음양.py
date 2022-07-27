8b858d6f22b72498fc394697207971266b3c1b78
JOO-10000-Zi <j4615545@gmail.com>
2022-07-27 22:08:39 +0900

    백준문제풀이

---
+++ 7월/baekjoon/1_PRO음양.py
@@ -0,0 +1,13 @@
+
+def solution(absolutes, signs):
+    answer = 0
+ 
+    
+    for i in range(len(absolutes)):
+        if signs[i] :
+            answer += absolutes[i]
+        else:
+            answer -= absolutes[i]
+    
+    
+    return answer
---
+++ 7월/baekjoon/5_BOJ7568.py
@@ -0,0 +1,5 @@
+H = int(input())
+
+
+for i in range(1, H+1):
+    a, b = map(int, input().split())
