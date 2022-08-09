bb82d930b2b33a39c7c89c95dde5d13258109cf1
JOO-10000-Zi <j4615545@gmail.com>
2022-08-09 23:59:57 +0900

    노션풀이

---
+++ baekjoon/no21.py
@@ -0,0 +1,20 @@
+import sys
+from pprint import pprint
+
+sys.stdin = open("no21.txt")
+
+N, M = map(int, input().split())
+
+graph = [[0] * (N+1) for _ in range(N+1)]
+
+graph_list = [[] for _ in range(N+1)]
+
+for _ in range(M):
+    v1, v2 = map(int, input().split())
+    graph[v1][v2] = 1
+    graph[v2][v1] = 1
+    graph_list[v1].append(v2)
+    graph_list[v2].append(v1)
+
+pprint(graph)
+print(graph_list)
