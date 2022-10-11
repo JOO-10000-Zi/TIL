6d5333ad1c1c3a5dcc3663b34148a17a3f8c6661
JOO-10000-Zi <j4615545@gmail.com>
2022-10-11 17:28:02 +0900

    회원가입 생성

---
+++ django/1011/pjt/accounts/forms.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.forms import UserCreationForm
+from .models import User
+
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
---
+++ django/1011/pjt/accounts/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect
+# from django.contrib.auth.forms import UserCreationForm
+from .forms import CustomUserCreationForm
+
+# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:index')
+    else:
+        form = CustomUserCreationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/signup.html', context)
