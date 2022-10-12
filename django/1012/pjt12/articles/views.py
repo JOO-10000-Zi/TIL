e77995d95e18caa7180788bfc583a7f137535f43
JOO-10000-Zi <j4615545@gmail.com>
2022-10-13 00:08:21 +0900

    게시판 기능 추가(복사)

--- django/1012/pjt12/articles/views.py
+++ django/1012/pjt12/articles/views.py
@@ -1,5 +1,71 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Reviews
 # Create your views here.
+
 def index(request):
-    return render(request, 'articles/index.html')
+    reviews = Reviews.objects.order_by("-id")
+
+    context = {
+        "Review": reviews,
+    }
+
+    return render(request, "articles/index.html", context)
+
+
+def create(request):
+
+    return render(request, "articles/create.html")
+
+
+def send(request):
+    title = request.GET.get("title")
+    content = request.GET.get("content")
+    created_at = request.GET.get("created_at")
+    update_at = request.GET.get("update_at")
+
+    Reviews.objects.create(
+        title=title,
+        content=content,
+        created_at=created_at,
+        update_at=update_at,
+    )
+
+    return redirect("articles:index")
+
+
+def detail(request, pk):
+    pk = Reviews.objects.get(id=pk)
+
+    context = {"id": pk}
+
+    return render(request, "articles/detail.html", context)
+
+
+def delete(request, pk):
+    pk = Reviews.objects.get(id=pk)
+    pk.delete()
+
+    return redirect("articles:index")
+
+
+def edit(request, pk):
+    pk = Reviews.objects.get(id=pk)
+
+    context = {"pk": pk}
+
+    return render(request, "articles/edit.html", context)
+
+
+def update(request, pk):
+    pk = Reviews.objects.get(id=pk)
+
+    title = request.GET.get("title")
+    content = request.GET.get("content")
+
+    pk.title = title
+    pk.content = content
+
+    pk.save()
+
+    return redirect("articles:detail", pk.pk)
+
