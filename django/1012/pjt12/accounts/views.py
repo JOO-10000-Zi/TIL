5a075454d09cfea846c82349be9c5081306667ef
JOO-10000-Zi <j4615545@gmail.com>
2022-10-12 23:39:52 +0900

    login 기능 구현 완료

--- django/1012/pjt12/accounts/views.py
+++ django/1012/pjt12/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from accounts.forms import CustomUserCreationForm
+from django.contrib.auth import login as auth_login
 from django.contrib.auth.forms import AuthenticationForm
 from .models import User
 
@@ -32,9 +33,13 @@ def detail(request, pk):
     return render(request, 'accounts/detail.html', context)
 
 def login(request):
-    # if request.method == 'POST':
-
-    form = AuthenticationForm()
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('articles:index')
+    else:
+        form = AuthenticationForm()
     context = {
         'form':form
     }
