430497c5833c2faebdb37197f6b202cb9942087e
JOO-10000-Zi <j4615545@gmail.com>
2022-10-18 14:20:50 +0900

    add Comment Model

--- django/1018/articles/models.py
+++ django/1018/articles/models.py
@@ -5,3 +5,8 @@ class Article(models.Model):
     title = models.CharField(max_length=80)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    content = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
