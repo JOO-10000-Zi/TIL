29423894ad895f11da5e3af31aa96f52ef2ab366
JOO-10000-Zi <j4615545@gmail.com>
2022-10-18 14:16:28 +0900

    add detail, update

--- django/1018/articles/urls.py
+++ django/1018/articles/urls.py
@@ -6,4 +6,6 @@ app_name = 'articles'
 urlpatterns =[
     path('', views.index, name='index'),
     path('create/', views.create, name='create'),
+    path('<int:pk>/detail/', views.detail, name='detail'),
+    path('<int:pk>/update/', views.update, name='update'),
 ]
--- django/1018/articles/views.py
+++ django/1018/articles/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Article
 from .forms import ArticleForm
 from django.contrib import messages
+
 # Create your views here.
 def index(request):
     articles = Article.objects.order_by('-pk')
@@ -22,3 +23,25 @@ def create(request):
         'form':form
     }
     return render(request, 'articles/create.html', context)
+
+def detail(request, pk):
+    articles = Article.objects.get(pk=pk)
+    context = {
+        'articles': articles,
+    }
+    return render(request, 'articles/detail.html', context)
+
+def update(request, pk):
+    articles = Article.objects.get(pk=pk)
+    if request.method == "POST":
+        form = ArticleForm(request.POST, request.FILES, instance=articles)
+        if form.is_valid():
+            form.save()
+            messages.success(request, '글 수정을 완료 했습니다.')
+            return redirect('articles:detail', articles.pk)
+    else:
+        form = ArticleForm(instance=articles)
+    context = {
+        'form': form
+    }
+    return render(request, 'articles/update.html', context)
