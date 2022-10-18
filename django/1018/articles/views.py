b8f4be838256e1c81a26a09b6b52e92df2fb485b
JOO-10000-Zi <j4615545@gmail.com>
2022-10-18 16:27:22 +0900

    add comment_delete,count

--- django/1018/articles/views.py
+++ django/1018/articles/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Article
+from .models import Article, Comment
 from .forms import ArticleForm, CommentForm
 from django.contrib import messages
 
@@ -27,9 +27,11 @@ def create(request):
 def detail(request, pk):
     articles = Article.objects.get(pk=pk)
     comment_form = CommentForm()
+    comments = articles.comment_set.all()
     context = {
         'articles': articles,
         'comment_form': comment_form,
+        'comments':comments,
     }
     return render(request, 'articles/detail.html', context)
 
@@ -47,3 +49,17 @@ def update(request, pk):
         'form': form
     }
     return render(request, 'articles/update.html', context)
+
+def comment_create(request, pk):
+    article = Article.objects.get(pk=pk)
+    comment_form = CommentForm(request.POST)
+    if comment_form.is_valid():
+        comment = comment_form.save(commit=False)
+        comment.article = article
+        comment.save()
+    return redirect('articles:detail', article.pk )
+
+def comment_delete(request, article_pk, comment_pk):
+    comment = Comment.objects.get(pk=comment_pk)
+    comment.delete()
+    return redirect('articles:detail', article_pk)
