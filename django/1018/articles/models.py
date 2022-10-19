2c01fe673b6ab137732a3cd7bf5a1274ad7e3a15
JOO-10000-Zi <j4615545@gmail.com>
2022-10-19 14:05:04 +0900

    user table add

--- django/1018/articles/models.py
+++ django/1018/articles/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class Article(models.Model):
     title = models.CharField(max_length=80)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
 class Comment(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
