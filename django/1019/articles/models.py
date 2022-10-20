13017bae6a3ec04f287ba2bb31663d7c3e7a8fb4
JOO-10000-Zi <j4615545@gmail.com>
2022-10-20 15:30:26 +0900

    add models Article, Comment

--- django/1019/articles/models.py
+++ django/1019/articles/models.py
@@ -4,3 +4,14 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     pass
+
+class Article(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=80)
+    content = models.TextField()
+    image = models.ImageField(upload_to='articles/')
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    content = models.CharField(max_length=80)
