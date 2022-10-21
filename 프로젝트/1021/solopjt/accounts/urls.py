07a787dc3545180efadfb916df9b74e841633840
JOO-10000-Zi <j4615545@gmail.com>
2022-10-21 14:47:32 +0900

    회원가입, 로그인 기능 구현

--- 프로젝트/1021/solopjt/accounts/urls.py
+++ 프로젝트/1021/solopjt/accounts/urls.py
@@ -5,4 +5,6 @@ app_name='accounts'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
 ]
--- 프로젝트/1021/solopjt/accounts/views.py
+++ 프로젝트/1021/solopjt/accounts/views.py
@@ -1,5 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import User
+from .forms import CustomUserCreationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
 def index(request):
-    return render(request, 'accounts/index.html')
+    accounts = User.objects.order_by('-pk')
+    context = {
+        'accounts': accounts
+    }
+    return render(request, 'accounts/index.html', context)
+
+def signup(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("accounts:index")
+    else:
+        form = CustomUserCreationForm()
+    context = {"form": form}
+    return render(request, "accounts/signup.html", context)
+
+def login(request):
+    if request.user.is_anonymous:
+        if request.method == "POST":
+            form = AuthenticationForm(request, data=request.POST)
+            if form.is_valid():
+                auth_login(request, form.get_user())
+                return redirect("accounts:index")
+        else:
+            form = AuthenticationForm()
+        context = {"form": form}
+        return render(request, "accounts/login.html", context)
+    else:
+        return redirect("accounts:index")
